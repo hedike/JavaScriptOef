175e8c49cbc38a417d7ff94454be2c1db70ddae9
hedike <hedike1@gmail.com>
2024-07-26 15:11:46 +0200

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+""" print("Hello world!")
+x = 33
+print("x") """
+
+length=float(input("what's your length?: "))
+# meter = int(length/100)
+# cm = length - (meter * 100)
+# print("Your length is : ", meter,",",cm," meter", sep="")
+print("Your length is : ", length/100, " meter")
